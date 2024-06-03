d48b69cb045b3130c36bf1ca53038276587c73ed
Cristian <zaccardicristian@gmail.com>
2024-06-02 21:09:18 -0300

    Entry 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Num1 = 0;
+            double Num2 = 0;
+            double Num3 = 0;
+
+            Console.WriteLine("Ingrese el primer número: ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número: ");
+            Num3 = Convert.ToInt32(Console.ReadLine());
+            double resultado = (Num1 + Num2 + Num3) / 3;
+            Console.WriteLine(resultado);
+        }
+    }
+}
